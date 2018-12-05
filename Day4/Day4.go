c09ed4048b5bbf1d5f1c88f19fd0bc644ecda8ff
Sean Kelly <seandtkelly@gmail.com>
2018-12-05 23:04:19 +1300

    Day4 hack job - better optimisation required

--- Day4/Day4.go
+++ Day4/Day4.go
@@ -6,15 +6,17 @@ import (
 	"time"
 )
 
-type XY struct {
-    X int
-    Y int
+type guardEvent struct {
+    minute int
+    asleep bool
 }
 
+type guardEventRange struct {
+    start int
+    stop int
+}
 
-
-
-func processEvents(input []string) {
+func processEvents(input []string, gaurdData map[int]map[time.Time][]guardEvent) {
 
 
     var GuardID int
@@ -40,28 +42,86 @@ func processEvents(input []string) {
             Month = int(dayTime.Month())
         }
 
-        fmt.Printf("%d %d ", dayTime.Unix(), EventTime)
-
-
         count,_ := fmt.Sscanf(EventString, "Guard #%d", &GuardID)
 
-        fmt.Printf("%d ", GuardID )
-        if count == 1 {
-            fmt.Printf("\n")
-        } else {
+        _, ok := gaurdData[GuardID]
+
+        if ok == false {
+            gaurdData[GuardID] = make(map[time.Time][]guardEvent)
+        }
+
+        _, ok = gaurdData[GuardID][dayTime]
+
+        if count != 1 {
+            guardE := guardEvent{Minute, false}
             if EventString == "wakes up" {
-               fmt.Printf("true\n")
+               guardE.asleep = false
             } else if EventString == "falls asleep" {
-               fmt.Printf("false\n")
+               guardE.asleep = true
             } else {
                 panic("Input error")
             }
+
+            gaurdData[GuardID][dayTime] = append(gaurdData[GuardID][dayTime], guardE)
         }
     }
+    
+    gaurdMinutes := make(map[int][]int)
+    maxMinutes := 0
+    maxMinutesValue := 0
+    maxGuard := 0
+    maxTotalGuard := 0
+    maxTotalGuardID := 0
+
+    for GuardID, allEvents := range gaurdData {
+        gaurdMinutes[GuardID] = make([]int, 120)
+        totalGuard := 0
+        for day,dayEvents := range allEvents {
+           fmt.Println(GuardID, " ", day, " ", dayEvents)
+           for i := 0; i < len(dayEvents); i+=2 {
+                start := dayEvents[i]
+                end := dayEvents[i+1]
+                if (start.asleep == true) && (end.asleep == false) {
+                    for ii := start.minute; ii < end.minute; ii++ {
+                        gaurdMinutes[GuardID][ii] += 1
+                        totalGuard += 1
+                        if gaurdMinutes[GuardID][ii] > maxMinutes {
+                            maxMinutes =  gaurdMinutes[GuardID][ii]
+                            maxMinutesValue = ii
+                            maxGuard = GuardID
+                        }
+                    }
+                } else {
+                    panic("Input error")
+                }
+           }
+        }
+        if totalGuard > maxTotalGuard {
+            maxTotalGuard = totalGuard
+            maxTotalGuardID = GuardID
+        }
+    }
+
+
+    maxTotalMinutesValue  := 0
+    maxTotalMinutes := 0
+
+    for i, minutes := range gaurdMinutes[maxTotalGuardID] {
+        if minutes > maxTotalMinutes {
+            maxTotalMinutesValue = i
+            maxTotalMinutes = minutes
+        }
+    }
+
+    fmt.Println("maxGuard ",maxTotalGuardID, " maxTotalMinutesValue", maxTotalMinutesValue, " answer ", maxTotalGuardID*maxTotalMinutesValue)
+    fmt.Println("maxGuard ",maxGuard, " maxMinutesValue", maxMinutesValue, " answer ", maxGuard*maxMinutesValue)
+
 }
 
 func main() {
     flag.Parse()
 
-    processEvents(flag.Args())
+    var guardData = map[int]map[time.Time][]guardEvent{}
+
+    processEvents(flag.Args(), guardData)
 }
